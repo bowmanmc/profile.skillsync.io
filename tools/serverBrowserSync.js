e8436cf62e6ac6a2ae7ceaa55d427b2dfa9142cd
Michael Bowman <bowmanmc@gmail.com>
2016-11-29 21:27:37 -0500

    browsersync target that reloads when ejs changes are made

---
+++ tools/serverBrowserSync.js
@@ -0,0 +1,25 @@
+import browserSync from 'browser-sync';
+import proxy from 'http-proxy-middleware';
+
+const proxyOpts = {
+    target: 'http://localhost:4000' // where express is running (src/bin/www)
+};
+
+browserSync({
+    'files': [
+        'src/**/*'
+    ],
+    'watchOptions': {
+        'ignoreInitial': true
+    },
+    'server': {
+        'baseDir': 'src',
+    },
+    'port': 9000,
+    'middleware': [proxy('/', proxyOpts)],
+    'reloadOnRestart': false,
+    'notify': false,
+    'open': false
+    //'open': 'local',
+    //'browser': 'chrome',
+});

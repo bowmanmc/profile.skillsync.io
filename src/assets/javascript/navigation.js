95955fbbae0cd713750d708b95610a230728cbc1
Michael Bowman <bowmanmc@gmail.com>
2016-12-12 18:02:06 -0500

    page navigation with scrolling animation

--- src/assets/javascript/navigation.js
+++ src/assets/javascript/navigation.js
@@ -4,9 +4,13 @@ import jQuery from 'jquery';
 
 const ANIMATE_TIME = 250; // milliseconds
 
-// Setup navigation event listeners... for ids, look in views/partials/navigation.ejs
-jQuery('#nav-introduction').click(function() {
-    jQuery('html, body').animate({
-        scrollTop: jQuery('#profile-introduction').offset().top
-    }, ANIMATE_TIME);
+// Setup navigation event listeners. <a class="scroll__link" ... /> tags
+jQuery('.scroll__link').each(function() {
+    const link = jQuery(this).first();
+    const hash = this.hash;
+    link.click(function() {
+        jQuery('html, body').animate({
+            scrollTop: jQuery(hash).offset().top
+        }, ANIMATE_TIME);
+    });
 });

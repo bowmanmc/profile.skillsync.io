d0798a8259f47cf4668351b957134f9b16c99168
Michael Bowman <bowmanmc@gmail.com>
2016-12-13 20:52:58 -0500

    responsive footer

--- src/assets/javascript/navigation.js
+++ src/assets/javascript/navigation.js
@@ -4,6 +4,7 @@ import jQuery from 'jquery';
 
 const ANIMATE_TIME = 250; // milliseconds
 
+
 // Setup navigation event listeners. <a class="scroll__link" ... /> tags
 jQuery('.scroll__link').each(function() {
     const link = jQuery(this).first();
